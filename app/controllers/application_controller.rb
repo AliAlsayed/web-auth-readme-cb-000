225b9187564ae745a8348474b3ff45ef0fa009c9
Ali Alsayed <a_alsayed87@hotmail.com>
2019-08-26 08:33:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
       redirect_uri = CGI.escape("http://localhost:3000/auth")
       foursquare_url = "https://foursquare.com/oauth2/authenticate?
       client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+      redirect_to foursquare_url unless logged_in?
     end
 
     def logged_in?
